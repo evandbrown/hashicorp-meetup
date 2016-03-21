c157ddc25410312553e86a23dab9067d486a410c
Evan Brown <evanbrown@google.com>
2016-03-20 20:26:39 -0700

    Add new network

--- network.tf
+++ network.tf
@@ -8,3 +8,10 @@ resource "google_compute_subnetwork" "default-us-east1" {
     network = "${google_compute_network.default.self_link}"
     region = "us-east1"
 }
+
+resource "google_compute_subnetwork" "default-us-central1" {
+    name = "default-us-central1"
+    ip_cidr_range = "192.168.0.0/16"
+    network = "${google_compute_network.default.self_link}"
+    region = "us-central1"
+}
