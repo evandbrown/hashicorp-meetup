f4d6e59ad27f195c6a1d4cc7a9d3ee4d8f8471a0
Evan Brown <evanbrown@google.com>
2016-03-20 20:50:48 -0700

    Add firewall

---
+++ firewall.tf
@@ -0,0 +1,11 @@
+resource "google_compute_firewall" "www" {
+    name = "www"
+    network = "${google_compute_network.default.name}"
+
+    allow {
+        protocol = "tcp"
+        ports = ["80", "443"]
+    }
+
+    target_tags = ["www"]
+}
