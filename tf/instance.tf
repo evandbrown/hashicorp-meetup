792489c5abf468092d9d95e0964e35503f65b15e
Evan Brown <evanbrown@google.com>
2016-03-21 20:54:42 -0700

    remove firewall

--- tf/instance.tf
+++ tf/instance.tf
@@ -22,16 +22,3 @@ resource "google_compute_instance" "default" {
 
     metadata_startup_script = "/usr/local/bin/app"
 }
-
-resource "google_compute_firewall" "www-demo" {
-    name = "www-demo"
-    network = "default"
-
-    allow {
-        protocol = "tcp"
-        ports = ["8080"]
-    }
-
-    target_tags = ["www"]
-    source_ranges = ["0.0.0.0/0"]
-}
