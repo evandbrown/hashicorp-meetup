9fd8961d91020340056c582fa40243c3ccbbfb30
Evan Brown <evanbrown@google.com>
2016-03-21 21:01:41 -0700

    remove route

--- tf/network.tf
+++ tf/network.tf
@@ -15,12 +15,3 @@ resource "google_compute_subnetwork" "default-us-central1" {
     network = "${google_compute_network.default.self_link}"
     region = "us-central1"
 }
-
-resource "google_compute_route" "default-floating-vip" {
-    name = "default-floating-vip"
-    dest_range = "10.1.1.1/32"
-    network = "${google_compute_network.default.name}"
-    next_hop_instance = "some-spof-instance"
-    next_hop_instance_zone = "us-central1-f"
-    priority = 100
-}
